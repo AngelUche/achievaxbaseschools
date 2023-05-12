a7bbe98d33cb1bef0886680c5add03522fa48a6b
AngelUche <ucheorji9974@gmail.com>
2023-05-12 10:58:11 +0100

    modified the navbar

--- src/components/Navigaration/Navbar.tsx
+++ src/components/Navigaration/Navbar.tsx
@@ -12,22 +12,22 @@ function Navbar() {
 
   return (
     <>
-      <nav className="fixed top-6 left-0 lg:left-[8%] inset-x-0 flex justify-between px-10 z-10">
+      <nav className="fixed top-6 left-0 lg:left-[8%] inset-x-0 flex justify-between px-10 z-10 md:hidden">
         <div className="md:w-[60px] md:h-[62px] w-[43px] h-[50px]">
           <img src={Logo} alt="img" className="block w-full h-full" />
         </div>
         {/* Mobile Hamburger Menu */}
         <div className={`md:hidden cursor-pointer`} onClick={handleToggleNav}>
           {isnavOpen ? (
-            <AiOutlineClose size={30} color="white" />
+            <AiOutlineClose size={30} color="grey" />
           ) : (
-            <AiOutlineMenu size={30} color="white" />
+            <AiOutlineMenu size={30} color="w" />
           )}
         </div>
       </nav>
       {/* Aside Navigation */}
       <div
-        className={`fixed z-0 md:hidden transition duration-[.9s] ease-in-out   ${
+        className={`fixed z-10 md:hidden transition duration-[.9s] inset-y-0 ease-in-out top-0  bg-[black] ${
           isnavOpen ? "translate-x-[0]" : "-translate-x-[120%]"
         } `}
       >
--- src/components/Navigaration/Navlinkes.tsx
+++ src/components/Navigaration/Navlinkes.tsx
@@ -4,6 +4,8 @@ import { AboutUs, Admissions, Academics, Portal } from "./NavLinkData";
 import { TbChevronUp } from "react-icons/tb";
 import "../../assets/styles/Navlinlks.css";
 import { useState } from "react";
+import Logo from "../../assets/Homepage/logo.png";
+
 const Navlinkes = () => {
   const [isShown, setIsShown] = useState<boolean>(false);
   const [isAcedmicShown, setSsAcedmicShown] = useState<boolean>(false);
@@ -11,185 +13,189 @@ const Navlinkes = () => {
   const [isPortalShown, setIsPortalShown] = useState<boolean>(false);
   return (
     <>
-      <div
-        className=" z-0 justify-center md:justify-start pt-[6rem] md:pt-0 md:items-center
-        flex md:flex-row md:gap-x-[1.8%] md:ml-[15%] text-white w-[250px] md:w-full bg-[black] md:bg-transparent
-        flex-col gap-y-14 md:gap-y-0 md:pl-[2%] md:h-[60px] pl-10 lg:gap-x-[3%] lg:ml-[20%]"
-      >
-        <div className="flex">
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              ` text-white  md:text-[14px] lg:text[16px]
+      <div className="fle gap-[3%] md:pl-[16%] h-full">
+        <div className="md:w-[60px] md:h-[62px] w-[43px] h-[50px] hidden md:block">
+          <img src={Logo} alt="img" className="block w-full h-full" />
+        </div>
+        <div
+          className=" z-10 justify-center md:justify-start md:items-center flex md:flex-row md:gap-x-[1.8%] text-white w-[250px] md:w-full
+        flex-col gap-y-[5rem] md:gap-y-0 md:pl-[10%] md:h-[60px] pl-10 lg:gap-x-[4%] pt-5 md:pt-0"
+        >
+          <div className="flex">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                ` text-white  md:text-[14px] lg:text[16px]
           ${isActive ? " border-t-2 " : undefined}`
-            }
-          >
-            <div className="relative">
-              <p className=" NavLinkshovereffect">Home</p>
-            </div>
-          </NavLink>
-        </div>
-        <li className="flex items-center  relative">
-          <NavLink
-            to="aboutus"
-            className={({ isActive }) =>
-              ` text-white  ${isActive ? " border-t-2 " : undefined}`
-            }
-          >
-            <div
-              className="md:text-[14px] lg:text[16px]  w-[70px] relative"
-              onMouseEnter={() => {
-                setIsShown(true);
-              }}
-              onMouseLeave={() => {
-                setIsShown(false);
-              }}
+              }
             >
-              <p className="NavLinkshovereffect">About Us</p>
-            </div>
-          </NavLink>
-          <TbChevronUp />
-          {isShown && (
-            <div className="absolute top-6 md:top-5 bg-white text-black left-[-20px] md:left-[-40px] text-sm leading-7 p-3 ">
-              {AboutUs.map((about) => {
-                return (
-                  <div key={about.id}>
-                    <a href={about.link}>{about.name}</a>
-                  </div>
-                );
-              })}
-            </div>
-          )}
-        </li>
-        <div
-          className="flex items-center relative"
-          onMouseEnter={() => {
-            setSsAcedmicShown(true);
-          }}
-          onMouseLeave={() => {
-            setSsAcedmicShown(false);
-          }}
-        >
-          <NavLink
-            to="academics"
-            className={({ isActive }) =>
-              ` text-white 
-                      ${isActive ? " border-t-2 " : undefined}`
-            }
-          >
-            <div className="md:text-[14px] lg:text[16px] relative">
-              <p className="NavLinkshovereffect">Acedemics</p>
-            </div>
-          </NavLink>
-          <div>
-            <TbChevronUp />
+              <div className="relative">
+                <p className=" NavLinkshovereffect">Home</p>
+              </div>
+            </NavLink>
           </div>
-          {isAcedmicShown && (
-            <div className="absolute top-6 md:top-5 bg-white text-black left-[-20px] md:left-[-30px] text-sm leading-7 p-3  w-max">
-              {Academics.map((about) => {
-                return (
-                  <div key={about.id}>
-                    <a href={about.link}>{about.name}</a>
-                  </div>
-                );
-              })}
-            </div>
-          )}
-        </div>
-        <div
-          className="flex items-center relative"
-          onMouseEnter={() => {
-            setIsAdmissionhown(true);
-          }}
-          onMouseLeave={() => {
-            setIsAdmissionhown(false);
-          }}
-        >
-          <NavLink
-            to="admission"
-            className={({ isActive }) =>
-              ` text-white 
-                      ${isActive ? " border-t-2 " : undefined}`
-            }
-          >
-            <div className="md:text-[14px] lg:text[16px] relative">
-              <p className="NavLinkshovereffect">Admissions</p>
-            </div>
-          </NavLink>
-          <div>
-            <TbChevronUp />
-          </div>
-          {isAdmissionhown && (
-            <div className="absolute top-6 md:top-5 bg-white text-black left-[-20px] md:left-[-30px] text-sm leading-7 p-3 w-max">
-              {Admissions.map((about) => {
-                return (
-                  <div key={about.id}>
-                    <a href={about.link}>{about.name}</a>
-                  </div>
-                );
-              })}
-            </div>
-          )}
-        </div>
-        <div
-          className="flex items-center relative"
-          onMouseEnter={() => {
-            setIsPortalShown(true);
-          }}
-          onMouseLeave={() => {
-            setIsPortalShown(false);
-          }}
-        >
-          <NavLink
-            to="portal"
-            className={({ isActive }) =>
-              ` text-white 
-                      ${isActive ? " border-t-2 " : undefined}`
-            }
-          >
-            <div className="md:text-[14px]  lg:text[16px] relative">
-              <p className="NavLinkshovereffect">Portal</p>
-            </div>
-          </NavLink>
-          <div>
-            <TbChevronUp />
-          </div>
-          {isPortalShown && (
-            <div className="absolute top-6 md:top-5 bg-white text-black left-[-20px] md:left-[-30px] text-sm leading-7 p-3  w-max">
-              {Portal.map((about) => {
-                return (
-                  <div key={about.id}>
-                    <a href={about.link}>{about.name}</a>
-                  </div>
-                );
-              })}
-            </div>
-          )}
-        </div>
-        <div className="flex gap-3 md:gap-4 mt-4 mb-[22px] md:mb-0 md:mt-0 lg:ml-16 items-center">
-          <div>
-            <Login
-              style={{
-                paddingLeft: " 6px 18px",
-                paddingRight: "18px",
-                paddingTop: "6px",
-                paddingBottom: "6px",
-              }}
+          <li className="flex items-center  relative">
+            <NavLink
+              to="aboutus"
+              className={({ isActive }) =>
+                ` text-white  ${isActive ? " border-t-2 " : undefined}`
+              }
             >
-              Login
-            </Login>
-          </div>
-          <div>
-            <SignUp
-              style={{
-                paddingLeft: "18px",
-                paddingRight: "18px",
-                paddingTop: "6px",
-                paddingBottom: "6px",
-              }}
+              <div
+                className="md:text-[14px] lg:text[16px]  w-[70px] relative"
+                onMouseEnter={() => {
+                  setIsShown(true);
+                }}
+                onMouseLeave={() => {
+                  setIsShown(false);
+                }}
+              >
+                <p className="NavLinkshovereffect">About Us</p>
+              </div>
+            </NavLink>
+            <TbChevronUp />
+            {isShown && (
+              <div className="absolute top-6 md:top-5 bg-white text-black left-[-20px] md:left-[-40px] text-sm leading-5 md:leading-7 p-3 ">
+                {AboutUs.map((about) => {
+                  return (
+                    <div key={about.id}>
+                      <a href={about.link}>{about.name}</a>
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+          </li>
+          <div
+            className="flex items-center relative"
+            onMouseEnter={() => {
+              setSsAcedmicShown(true);
+            }}
+            onMouseLeave={() => {
+              setSsAcedmicShown(false);
+            }}
+          >
+            <NavLink
+              to="academics"
+              className={({ isActive }) =>
+                ` text-white 
+                      ${isActive ? " border-t-2 " : undefined}`
+              }
             >
-              Sign Up
-            </SignUp>
+              <div className="md:text-[14px] lg:text[16px] relative">
+                <p className="NavLinkshovereffect">Acedemics</p>
+              </div>
+            </NavLink>
+            <div>
+              <TbChevronUp />
+            </div>
+            {isAcedmicShown && (
+              <div className="absolute top-6 md:top-5 bg-white text-black left-[-20px] md:left-[-30px] text-sm leading-5 md:leading-7p-3  w-max">
+                {Academics.map((about) => {
+                  return (
+                    <div key={about.id}>
+                      <a href={about.link}>{about.name}</a>
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+          </div>
+          <div
+            className="flex items-center relative"
+            onMouseEnter={() => {
+              setIsAdmissionhown(true);
+            }}
+            onMouseLeave={() => {
+              setIsAdmissionhown(false);
+            }}
+          >
+            <NavLink
+              to="admission"
+              className={({ isActive }) =>
+                ` text-white 
+                      ${isActive ? " border-t-2 " : undefined}`
+              }
+            >
+              <div className="md:text-[14px] lg:text[16px] relative">
+                <p className="NavLinkshovereffect">Admissions</p>
+              </div>
+            </NavLink>
+            <div>
+              <TbChevronUp />
+            </div>
+            {isAdmissionhown && (
+              <div className="absolute top-6 md:top-5 bg-white text-black left-[-20px] md:left-[-30px] text-sm leading-5 md:leading-7 p-3 w-max">
+                {Admissions.map((about) => {
+                  return (
+                    <div key={about.id}>
+                      <a href={about.link}>{about.name}</a>
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+          </div>
+          <div
+            className="flex items-center relative"
+            onMouseEnter={() => {
+              setIsPortalShown(true);
+            }}
+            onMouseLeave={() => {
+              setIsPortalShown(false);
+            }}
+          >
+            <NavLink
+              to="portal"
+              className={({ isActive }) =>
+                ` text-white 
+                      ${isActive ? " border-t-2 " : undefined}`
+              }
+            >
+              <div className="md:text-[14px]  lg:text[16px] relative">
+                <p className="NavLinkshovereffect">Portal</p>
+              </div>
+            </NavLink>
+            <div>
+              <TbChevronUp />
+            </div>
+            {isPortalShown && (
+              <div className="absolute top-6 md:top-5 bg-white text-black left-[-20px] md:left-[-30px] text-sm leading-5 md:leading-7 p-3  w-max">
+                {Portal.map((about) => {
+                  return (
+                    <div key={about.id}>
+                      <a href={about.link}>{about.name}</a>
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+          </div>
+          <div className="flex gap-3 md:gap-4 mt-4 mb-[22px] md:mb-0 md:mt-0 lg:ml-[10%] items-center">
+            <div>
+              <Login
+                style={{
+                  paddingLeft: " 6px 18px",
+                  paddingRight: "18px",
+                  paddingTop: "6px",
+                  paddingBottom: "6px",
+                }}
+              >
+                Login
+              </Login>
+            </div>
+            <div>
+              <SignUp
+                style={{
+                  paddingLeft: "18px",
+                  paddingRight: "18px",
+                  paddingTop: "6px",
+                  paddingBottom: "6px",
+                }}
+              >
+                Sign Up
+              </SignUp>
+            </div>
           </div>
         </div>
       </div>
